01adccc9e06ef097c4bc2ba162d769fa6bf57f24
paul-tes <pauasca8462@gmail.com>
2023-09-19 17:43:23 +0300

    Add sample datas in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,11 @@
+greetings = [
+  'Greetings',
+  'Hey!',
+  'What\'s up',
+  'How you holdin up?',
+  'Hello there!'
+]
 
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-Greeting.create(text: 'Greetings')
-Greeting.create(text: 'Hey!')
-Greeting.create(text: 'Whats up')
-Greeting.create(text: 'How you holdin up?')
-Greeting.create(text: 'Hello there!')
+greetings.each do |text|
+  Greeting.create(text: text)
+end
